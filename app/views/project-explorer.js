a344b7c3b5eac065ee4c24d5fd6b6a7147cf886a
mmwebster <milo.webster@gmail.com>
2015-08-09 20:54:38 -0700

    base layering system in place, about to create views for each layer to apply classBindings

---
+++ app/views/project-explorer.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.View.extend({
+  classNames: ['project-explorer'],
+  // actions: {
+  //   focus: function(level, order) {
+  //
+  //   },
+  // },
+});
