16ff8add9e5ca616b23ec3b3fd52268bb22b8ba9
mmwebster <milo.webster@gmail.com>
2015-08-09 21:29:07 -0700

    template bound class attributes in place (mostly)

---
+++ app/controllers/project-explorer.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  origin1a1EnabledL1: true,
+  origin2EnabledL2: true,
+});
--- app/views/project-explorer.js
+++ app/views/project-explorer.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.View.extend({
   classNames: ['project-explorer'],
+  origin1a1EnabledL1: true,
+  origin2EnabledL2: true,
   // actions: {
   //   focus: function(level, order) {
   //
