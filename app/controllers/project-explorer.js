034336c4e61cd2376ced4efbfc6c58c1ce71f462
mmwebster <milo.webster@gmail.com>
2015-08-11 00:24:05 -0700

    renovated project-explorer animations

--- app/controllers/project-explorer.js
+++ app/controllers/project-explorer.js
@@ -9,35 +9,43 @@ export default Ember.Controller.extend({
   pathTree: [{'layer':1, 'order':1}],
 
   negatePreviousSettings: function() {
-    this.set('origin' + this.get('currentLayerTop') + 'a' + this.get('currentOrderTop') + 'EnabledL1', false);
-    this.set('origin' + this.get('currentLevelBottom') + 'EnabledL2', false);
+    // console.log('FIRST' + 'origin' + this.get('pathTree.lastObject.layer') + 'a' + this.get('pathTree.lastObject.order') + 'EnabledL1');
+    // console.log('SECOND' + 'origin' + (this.get('pathTree.lastObject.layer')+1) + 'EnabledL2');
+    // debugger
+    this.set('origin' + this.get('pathTree.lastObject.layer') + 'a' + this.get('pathTree.lastObject.order') + 'EnabledL1', false);
+    this.set('origin' + (this.get('pathTree.lastObject.layer')+1) + 'EnabledL2', false);
   },
 
   actions: {
-    // zoom in on the selected level and order
-    focus: function(level, order) {
-      // save hierarchy 
-      this.get('pathTree').addObject({'level':level, 'order':order});
+    // zoom in on the selected layer and order
+    focus: function(layer, order) {
       // set previous settings to false
       this.negatePreviousSettings();
+      // save hierarchy 
+      this.get('pathTree').addObject({'layer':layer, 'order':order});
       // set new settings to true
-      this.set('origin' + level + 'a' + order + 'EnabledL1', true);
-      this.set('origin' + (level+1) + 'EnabledL2', true);
+      this.set('origin' + layer + 'a' + order + 'EnabledL1', true);
+      this.set('origin' + (layer+1) + 'EnabledL2', true);
       // save in the new "current" settings
-      this.set('currentLayerTop', level);
+      this.set('currentLayerTop', layer);
       this.set('currentOrderTop', order);
-      this.set('currentLevelBottom', level+1);
+      this.set('currentLevelBottom', layer+1);
     },
     blur: function() {
-      this.negatePreviousSettings();
-      //destroy previous pathTree object 
-      this.get('pathTree').removeObject(this.get('pathTree.lastObject'));
-      // set new settings to true
-      this.set('origin' + this.get('pathTree.lastObject.layer') + 'a' + this.get('pathTree.lastObject.order') + 'EnabledL1', true);
-      this.set('origin' + (this.get('pathTree.lastObject.layer')+1) + 'EnabledL2', true);
-      // debugger
-      // save in the new "current" settings
-      // remove object from path hierarchy history
+      if (this.get('pathTree.lastObject') != this.get('pathTree.firstObject')) {
+        // set previous settings to false
+        this.negatePreviousSettings();
+        //destroy previous pathTree object 
+        this.get('pathTree').removeObject(this.get('pathTree.lastObject'));
+        // set new settings to true
+        this.set('origin' + this.get('pathTree.lastObject.layer') + 'a' + this.get('pathTree.lastObject.order') + 'EnabledL1', true);
+        this.set('origin' + (this.get('pathTree.lastObject.layer')+1) + 'EnabledL2', true);
+        // debugger
+        // save in the new "current" settings
+        // remove object from path hierarchy history
+      } else {
+        console.warn('Cannot blur, at top of hierarchy.');
+      }
     },
   },
 });
