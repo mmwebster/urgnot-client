0cf0015e716d0fb1c473249d485930e07cd04410
mmwebster <milo.webster@gmail.com>
2015-08-23 00:00:20 -0700

    stable(ish), improved reliance on native ember component property bindings

--- app/components/explorer-entity.js
+++ app/components/explorer-entity.js
@@ -1,50 +1,94 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  // bindings
   classNames: ['entity'],
-  classNameBindings: ['type', 'order', 'layer1:layer-1', 'layer2:layer-2'],
+  classNameBindings: ['type', 'id', 'index', 'layer1:layer-1', 'layer2:layer-2', 'blurParent:parent-blur', 'isRoot:is-root'],
   attributeBindings: ['wrapperStyle:style'],     
+  // properties
   layer1: false,
   layer2: false,
-  entityRotation: null,
+  blurredParent: false,
+  blurredSibling: false,
+
+  id: function() {
+    return "id-" + this.get('node.id');
+  }.property('node.id'),
+
+  index: function() {
+    return "index-" + this.get('nodeIndex');
+  }.property('nodeIndex'),
 
   type: function() {
-    return "type-" + this.get('entity.type');
-  }.property('entity.type'),
+    return "type-" + this.get('node.type');
+  }.property('node.type'),
 
-  order: function() {
-    return "order-" + this.get('entityOrder');
-  }.property('entityOrder'),
+  // siblings: function() {
+  //   var parent = this.get('parent');
+  //   if (parent) {
+  //     return parent.children;
+  //   } else {
+  //     return this.get('node');
+  //   }
+  // }.property('parent'),
 
   wrapperStyle: function() {
     // degree increments for each sibling
-    var rotationIncrements = (360 / this.get('numSiblings'));
-    var rotation = rotationIncrements * this.get('entityOrder');
+    var rotationIncrements = (360 / this.get('siblings.length'));
+    var rotation = rotationIncrements * this.get('index');
     // save out for use in content rotation correction
-    this.set('entityRotation', rotation);
+    this.set('rotation', rotation);
     // return styling
     return "transform: rotate(" + rotation + "deg) !important;";
-  }.property(),
-       
+  }.property('siblings.length', 'index'),
+
   willInsertElement: function() {
-    var type = this.get('entity.type');
+    debugger;
+    // insert all component properties into the js object's properties hash
+    var type = this.get('node.type');
     if (type === 'star') {
       this.set('layer1', true);
     } else if (type === 'planet') {
       this.set('layer2', true);
     }
-    console.log("Inserted component of (" + type + ")");
+    console.log("Inserted component of (" + type + ").");
   },
 
   actions: {
 
-    // focus if entity is at layer 2
     focus: function() {
+      // focus if entity is at layer 2
       if (this.get('layer2')) {
-        var type = this.get('entity.type');
-        console.log('clicked ' + type + ':' + this.get('entityOrder'));
+        debugger;
+        console.log('clicked ' + this.get('entity.type') + ':' + this.get('entityOrder'));
+        var entity = this.get('entity');
+        // focus node
+        this.set('layer2', false);
+        this.set('layer1', true);
+        // var target = ".id-" + entity.id;
+        // $(target).removeClass('layer-2');
+        // $(target).addClass('layer-1');
+        // focus chidren
+        var children = entity.children || {};
+        for (var i = 0; i < children.length; i++) {
+          target = ".id-" + children[i].id;
+          $(target).addClass('layer-2');
+        }
+        // blur parent node
+        var target = ".id-" + this.get('parent.id');
+        $(target).addClass('parent-blur');
+        $(target).removeClass('layer-1');
+        // blur sibling nodes
+        var target;
+        var siblings = this.get('siblings') || {};
+        for (var i = 0; i < siblings.length; i++) {
+          if (siblings[i].id !== entity.id) {
+            target = ".id-" + siblings[i].id;
+            $(target).addClass('parent-blur');
+            $(target).removeClass('layer-2');
+          }
+        }
       }
     },
-
   },
 });
--- app/routes/app.js
+++ app/routes/app.js
@@ -4,38 +4,70 @@ export default Ember.Route.extend({
   routeModel: {
     // top level entity is project level
     entity: {
+      id: 1,
       name: 'Star',
       type: 'star',
       children: [ 
         {
+          id: 2,
           name: 'Planet 0',
           type: 'planet',
           children: [
             {
+              id: 3,
               name: 'moon 1',
               type: 'moon',
               children: [
                 {
+                  id: 4,
                   name: 'crater 1',
                   type: 'crater'
                 },
                 {
+                  id: 5,
                   name: 'crater 2',
                   type: 'crater'
                 }
               ]
             },
             {
+              id: 5,
+              name: 'moon 2',
+              type: 'moon'
+            },
+            {
+              id: 5,
+              name: 'moon 2',
+              type: 'moon'
+            },
+            {
+              id: 5,
+              name: 'moon 2',
+              type: 'moon'
+            },
+            {
+              id: 5,
+              name: 'moon 2',
+              type: 'moon'
+            },
+            {
+              id: 5,
               name: 'moon 2',
               type: 'moon'
             }
+
+
+
+
           ]
         },
         {
+          id: 6,
           name: 'Planet 1',
           type: 'planet'
         },
         {
+          id: 7,
           name: 'Planet 2',
           type: 'planet'
         }
