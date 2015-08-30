516c6ccd574c8615cb5f797e3af7343ff1bfb858
mmwebster <milo.webster@gmail.com>
2015-08-30 11:06:27 -0700

    setup settings route

--- app/routes/app.js
+++ app/routes/app.js
@@ -1,11 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  beforeModel: function() {
-    return this.get("session").fetch().catch(function() {});
-  },
-  model: function() {
-    var uid = this.controllerFor('application').get('currentUser.uid');
-    return this.store.find('user', uid);
-  },
 });
--- app/routes/app/settings.js
+++ app/routes/app/settings.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    var uid = this.controllerFor('application').get('currentUser.uid');
+    return this.store.find('user', uid);
+  },
 });
