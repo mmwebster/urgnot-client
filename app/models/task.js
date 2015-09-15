596b49588f182d12670ac6849225c0909a12ea75
mmwebster <milo.webster@gmail.com>
2015-09-14 18:38:36 -0700

    cleaning up model clutter

--- app/models/task.js
+++ app/models/task.js
@@ -4,8 +4,6 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   description: DS.attr('string'),
   node: DS.belongsTo('node'),
-  // assignedTo: DS.belongsTo('user'),
-  // assignedOnto: DS.belongsTo('node'),
   actionType: DS.attr('string'),
   actionData: DS.attr('string')
 });
