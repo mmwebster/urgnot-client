00afca4868f915ebb0b83fa3a9b8b45a3b9bd3ca
mmwebster <milo.webster@gmail.com>
2015-09-15 19:06:44 -0700

    student user functional onboarding complete

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -25,10 +25,16 @@ export default Ember.Controller.extend({
           email: this.get('email'),
           password: this.get('password')
         }).then(function(data) {
-          Ember.debug(data.currentUser);
-          var uid = _this.get('controllers.application.currentUser.uid');
-          _this.store.find('user', uid).catch(function() {
-            // no user record found create one
+          Ember.debug("Firebase user is '" + data.currentUser.email + "' w/ uid: '" + data.uid + "'.");
+          _this.store.find('user', data.uid).then(function(user) {
+            // user record found
+            Ember.debug("User record found for '" + user.get('email') + "'");
+            // Transitioning to app index
+            Ember.debug("Transitioning to app index.");
+            _this.transitionToRoute('app');
+          }, function(reason) {
+            // user record not found (not catching any other errors at the moment)
+            Ember.debug('No user record found; creating user record.');
             var newUser = _this.store.createRecord('user', {
               id: data.uid,
               email: data.currentUser.email
@@ -38,27 +44,39 @@ export default Ember.Controller.extend({
             _this.store.find('organization', {
               orderBy: 'email',
               equalTo: 'ucsc.edu'
-            }).then(function(records) {
+            }).then(function(orgs) {
+              // Retrieved records under the given email domain
               var emailDomain = data.currentUser.email.split('@')[1];
-              records.forEach(function(org) {
+              Ember.debug("Retrieved records under the domain '" + emailDomain + "'.");
+              // search for mappable org
+              orgs.forEach(function(org) {
                 if(org.get('email') === emailDomain) {
                   // found mapped organization, now add it to the user and save
+                  Ember.debug("Mapped organization with domain '" + emailDomain + "'.");
                   newUser.get('endOrganizations').addObject(org);
-                  org.save().then(function() {
-                    Ember.debug('Created new user.');
+                  newUser.set('activeOrganizationId', org.get('id'));
+                  var saved = org.save().then(function() {
+                    // New user saving with inverse relationship
+                    Ember.debug("Created new user '" + newUser.get('email') + "'.");
                     return newUser.save();
+                  });
+                  saved.then(function() {
+                    // Transitioning to app index
+                    Ember.debug("Transitioning to app index.");
+                    _this.transitionToRoute('app');
                   });
                 }
               });
+            }, function(reason) {
+              // Could not find an organization to map with.
+              Ember.debug('Could not find an organization to map with.');
+              alert("Could not find an organization to map with: " + reason.message);
             });
-
-          }).then(function() {
-            // user record found or now created
-            _this.transitionToRoute('app');
           });
 
         }, function() {
           console.warn('Incorrect credentials');
+          alert("You entered an incorrect username or password.");
         });
       } else {
         console.warn('User is already authenticated');
--- app/routes/app/index.js
+++ app/routes/app/index.js
@@ -1,16 +1,48 @@
 import Ember from 'ember';
 
+/*
+ * NOTES: Should abstract the various promises in the model out into the main body of the route 
+ * */
+
 export default Ember.Route.extend({
   model: function() {
     var _this = this;
     return new Ember.RSVP.Promise(function(resolve) {
       var model = {};
       var uid = _this.controllerFor('application').get('currentUser.uid');
+      // GET USER
       _this.store.find('user', uid).then(function(user) {
+        // SET USER
         model.user = user;
-        _this.store.find('project', user.get('activeProjectId')).then(function(project) {
-          model.project = project;
-          resolve(model);
+        // GET ORGANIZATION
+        _this.store.find('organization', user.get('activeOrganizationId')).then(function(org) {
+          // SET ORGANIZATION
+          model.org = org;
+          if (user.get('activeProjectId')) {
+            // GET PROJECT
+            _this.store.find('project', user.get('activeProjectId')).then(function(project) {
+              // SET PROJECT
+              model.project = project;
+              resolve(model);
+            });
+          } else {
+            // CREATE PROJECT
+            alert("Welcome to Urgnot!"); // temp welcome message
+            var newProject = _this.store.createRecord('project', {
+              name: "My 2015-2016 ISEF Project",
+              author: user,
+              organization: org
+            });
+            user.get('projects').addObject(newProject);
+            var saved = newProject.save().then(function(project) {
+              model.project = project;
+              user.set('activeProjectId', project.get('id'));
+              return user.save();
+            });
+            saved.then(function(some) {
+              resolve(model);
+            });
+          }
         });
       });
     });
